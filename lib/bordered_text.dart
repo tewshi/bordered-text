d291bda10e0bea52fd668717caba060b42ec4796
Joseph Michael <tjoe4u@gmail.com>
2019-06-13 18:40:56 +0100

    updates main library

--- lib/bordered_text.dart
+++ lib/bordered_text.dart
@@ -1,7 +1,90 @@
 library bordered_text;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/widgets.dart';
+
+/// Adds stroke to text widget
+/// {@tool sample}
+/// We can apply a very thin and subtle stroke to a [Text]
+/// ```dart
+/// BorderedText(
+///   strokeWidth: 1.0,
+///   text: Text(
+///     'Bordered Text',
+///     style: TextStyle(
+///       decoration: TextDecoration.none,
+///       decorationStyle: TextDecorationStyle.wavy,
+///       decorationColor: Colors.red,
+///     ),
+///   ),
+/// )
+/// ```
+/// {@end-tool}
+class BorderedText extends StatelessWidget {
+  BorderedText({
+    this.strokeCap = StrokeCap.round,
+    this.strokeJoin = StrokeJoin.round,
+    this.strokeWidth = 6.0,
+    this.strokeColor = const Color.fromRGBO(53, 0, 71, 1),
+    @required this.child,
+  }) : assert(child != null);
+
+  /// the stroke cap style
+  final StrokeCap strokeCap;
+
+  /// the stroke joint style
+  final StrokeJoin strokeJoin;
+
+  /// the stroke width
+  final double strokeWidth;
+
+  /// the stroke color
+  final Color strokeColor;
+
+  /// the `Text` widget to apply stroke on
+  final Text child;
+
+  @override
+  Widget build(BuildContext context) {
+    TextStyle style;
+    if (child.style != null) {
+      style = child.style.copyWith(
+        foreground: Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeCap = strokeCap
+          ..strokeJoin = strokeJoin
+          ..strokeWidth = strokeWidth
+          ..color = strokeColor,
+        color: null,
+      );
+    } else {
+      style = TextStyle(
+        foreground: Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeCap = strokeCap
+          ..strokeJoin = strokeJoin
+          ..strokeWidth = strokeWidth
+          ..color = strokeColor,
+      );
+    }
+    return Stack(
+      alignment: Alignment.center,
+      textDirection: TextDirection.ltr,
+      children: <Widget>[
+        Text(
+          child.data,
+          style: style,
+          maxLines: child.maxLines,
+          overflow: child.overflow,
+          semanticsLabel: child.semanticsLabel,
+          softWrap: child.softWrap,
+          strutStyle: child.strutStyle,
+          textAlign: child.textAlign,
+          textDirection: child.textDirection,
+          textScaleFactor: child.textScaleFactor,
+          textWidthBasis: child.textWidthBasis,
+        ),
+        child,
+      ],
+    );
+  }
 }
